738b0f50c00646cf70796380aae10bad7d2e5e3e
alej-mocse1 <alejandromcse01@gmail.com>
2023-06-19 11:50:53 -0300

    actualizacion en la informacion

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-
-
 let menu = document.getElementById('menu');
 let toggle_open = document.getElementById('toggle-open');
 
@@ -9,3 +7,27 @@ toggle_open.addEventListener('click', toggleMenu);
 function toggleMenu() {
   menu.classList.toggle('show-menu');
 }
+
+
+
+
+
+const typed = new Typed('.typed', {
+	strings: [
+		'<i class="titledinamico">Front end developer</i>',
+		'<i class="titledinamico">Back end developer</i>',
+	],
+
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
